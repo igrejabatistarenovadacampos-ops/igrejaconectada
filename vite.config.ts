d176f321a65310fdc7302fb2604b8e1c6e3956e3
igrejabatistarenovadacampos-ops <igrejabatistarenovadacampos@gmail.com>
2026-02-22 15:26:22 -0300

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,6 @@
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  root: 'src',
-  plugins: [react()],
-  build: {
-    outDir: '../dist',
-    emptyOutDir: true
-  }
+  plugins: [react()]
 })
